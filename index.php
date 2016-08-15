472687c7fd19b9ace572b8d74e5b2b3574c1a460
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-08-15 14:53:29 +0700

    update readme.md

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>d3 crossfilter dc</title>
+</head>
+<body>
+<h1>Draw chart much more easy with [d3, crossfilter, dc]</h1>
+<p><a href="contribution-calendar.html">github contribution calendar</a></p>
+<p><a href="order.html">order</a></p>
+</body>
+</html>
